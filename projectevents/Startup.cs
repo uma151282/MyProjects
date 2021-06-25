f4ff4873bf86c08f77759fee64b0772488b2ae03
ua <a@a.in>
2021-06-25 16:02:43 +0530

    commit test
    coomit

--- projectevents/Startup.cs
+++ projectevents/Startup.cs
@@ -15,7 +15,7 @@ using projectevents.Interfaces;
 using projectevents.Data;
 using Newtonsoft.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
+testt
 namespace projectevents
 {
     public class Startup
